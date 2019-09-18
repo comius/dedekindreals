398d8825256504f1bf44185f53e121d7334df6ac
Ivo List <ivo.list@guest.arnes.si>
2019-09-18 08:53:14 +0200

    Reorganized evaluation/refinement.

--- src/main/scala/com/github/comius/reals/Eval.scala
+++ src/main/scala/com/github/comius/reals/Eval.scala
@@ -30,12 +30,68 @@ object Eval {
   import AproximateSimple._
   import com.github.comius.floats.Floats.{ impl => D }
 
-  trait Refine[-A, +B] {
-    def refine(e: A)(implicit ctx: Context[VarDomain]): B
+  trait Evaluator {
+    def refineCut(cut: Cut)(implicit ctx: Context[VarDomain]): Cut
+    def refineExists(exists: Exists)(implicit ctx: Context[VarDomain]): Formula 
+    def refineForall(forall: Forall)(implicit ctx: Context[VarDomain]): Formula
+    
+      def refine(formula: Formula)(implicit ctx: Context[VarDomain]): Formula = {
+        approximate(formula)(ctx) match {
+          case Approximation(true, _)  => ConstFormula(true)
+          case Approximation(_, false) => ConstFormula(false)
+          case _ =>
+            formula match {
+    
+              case Less(x, y) => Less(refine(x), refine(y))
+    
+              case exists: Exists =>                
+                refineExists(exists)            
+              case forall: Forall =>                
+                refineForall(forall)
+              case And(x, y) =>
+                refine(x) match {
+                  case ConstFormula(true)  => refine(y)
+                  case ConstFormula(false) => ConstFormula(false)
+                  case a => refine(y) match {
+                    case ConstFormula(true)  => a
+                    case ConstFormula(false) => ConstFormula(false)
+                    case b                   => And(a, b)
+                  }
+                }
+              case Or(x, y) =>
+                refine(x) match {
+                  case ConstFormula(true)  => ConstFormula(true)
+                  case ConstFormula(false) => refine(y)
+                  case a => refine(y) match {
+                    case ConstFormula(true)  => ConstFormula(true)
+                    case ConstFormula(false) => a
+                    case b                   => Or(a, b)
+                  }
+                }
+              case c: ConstFormula => c
+            }
+        }
+      }
+    
+      
+      def refine(expr: Real)(implicit ctx: Context[VarDomain]): Real = expr match {
+        case cut: Cut =>           
+          refineCut(cut)
+        case Integrate(x, a, b, e) =>
+          val m = a.split(b)
+          val le = refine(e)(ctx + (x -> CutDomain(a, m)))
+          val ue = refine(e)(ctx + (x -> CutDomain(m, b)))
+          Add(Integrate(x, a, m, le), Integrate(x, m, b, ue))
+        case Add(x, y) => Add(refine(x), refine(y))
+        case Sub(x, y) => Sub(refine(x), refine(y))
+        case Mul(x, y) => Mul(refine(x), refine(y))
+        case Div(x, y) => Div(refine(x), refine(y))
+        case x         => x
+      }
   }
   
-  implicit object RefineCut extends Refine[Cut, Cut] {  
-    def refine(cut: Cut)(implicit ctx: Context[VarDomain]): Cut = {
+  object NewtonEvaluator extends Evaluator {
+    def refineCut(cut: Cut)(implicit ctx: Context[VarDomain]): Cut = {
         val Cut(x, a, b, l, u) = cut
         
         val (m1, m2) = a.trisect(b, ctx.roundingContext.up.getPrecision)
@@ -52,91 +108,34 @@ object Eval {
         val an = a2.max(a3)
         val bn = b2.min(b3)
   
-        Cut(x, an, bn, Eval.refine(l)(ctx + (x -> CutDomain(an, bn))), Eval.refine(u)(ctx + (x -> CutDomain(an, bn))))
+        Cut(x, an, bn, refine(l)(ctx + (x -> CutDomain(an, bn))), refine(u)(ctx + (x -> CutDomain(an, bn))))
     }
-  }
   
-  implicit object RefineExists extends Refine[Exists, Formula] {  
-    def refine(exists: Exists)(implicit ctx: Context[VarDomain]): Formula = {
+    
+    
+    def refineExists(exists: Exists)(implicit ctx: Context[VarDomain]): Formula = {
       val Exists(x, a, b, phi) = exists
       val m = a.split(b)
-      val phi2 = Eval.refine(phi)(ctx + (x -> WholeDomain(a, b)))
+      val phi2 = refine(phi)(ctx + (x -> WholeDomain(a, b)))
       if (phi2.isInstanceOf[ConstFormula])
         phi2
       else
         Or(Exists(x, a, m, phi2), Exists(x, m, b, phi2))
     }
-  }
-
-  implicit object RefineForall extends Refine[Forall, Formula] {  
-    def refine(forall: Forall)(implicit ctx: Context[VarDomain]): Formula = {
+  
+    def refineForall(forall: Forall)(implicit ctx: Context[VarDomain]): Formula = {
       val Forall(x, a, b, phi) = forall
       val m = a.split(b)
-      val phi2 = Eval.refine(phi)(ctx + (x -> WholeDomain(a, b)))
+      val phi2 = refine(phi)(ctx + (x -> WholeDomain(a, b)))
       if (phi2.isInstanceOf[ConstFormula])
         phi2
       else
         And(Forall(x, a, m, phi2), Forall(x, m, b, phi2))
-     }
+     }    
   }
 
+  val evaluator: Evaluator = NewtonEvaluator
   
-  def refine(formula: Formula)(implicit ctx: Context[VarDomain]): Formula = {
-    approximate(formula)(ctx) match {
-      case Approximation(true, _)  => ConstFormula(true)
-      case Approximation(_, false) => ConstFormula(false)
-      case _ =>
-        formula match {
-
-          case Less(x, y) => Less(refine(x), refine(y))
-
-          case exists: Exists =>
-            val r = implicitly[Refine[Exists,Formula]]
-            r.refine(exists)            
-          case forall: Forall =>
-            val r = implicitly[Refine[Forall,Formula]]
-            r.refine(forall)
-          case And(x, y) =>
-            refine(x) match {
-              case ConstFormula(true)  => refine(y)
-              case ConstFormula(false) => ConstFormula(false)
-              case a => refine(y) match {
-                case ConstFormula(true)  => a
-                case ConstFormula(false) => ConstFormula(false)
-                case b                   => And(a, b)
-              }
-            }
-          case Or(x, y) =>
-            refine(x) match {
-              case ConstFormula(true)  => ConstFormula(true)
-              case ConstFormula(false) => refine(y)
-              case a => refine(y) match {
-                case ConstFormula(true)  => ConstFormula(true)
-                case ConstFormula(false) => a
-                case b                   => Or(a, b)
-              }
-            }
-          case c: ConstFormula => c
-        }
-    }
-  }
-
-  def refine(expr: Real)(implicit ctx: Context[VarDomain]): Real = expr match {
-    case cut: Cut => 
-      val r = implicitly[Refine[Cut, Real]]
-      r.refine(cut)
-    case Integrate(x, a, b, e) =>
-      val m = a.split(b)
-      val le = refine(e)(ctx + (x -> CutDomain(a, m)))
-      val ue = refine(e)(ctx + (x -> CutDomain(m, b)))
-      Add(Integrate(x, a, m, le), Integrate(x, m, b, ue))
-    case Add(x, y) => Add(refine(x), refine(y))
-    case Sub(x, y) => Sub(refine(x), refine(y))
-    case Mul(x, y) => Mul(refine(x), refine(y))
-    case Div(x, y) => Div(refine(x), refine(y))
-    case x         => x
-  }
-
   def eval(expr: Real, precision: Int): Unit = {
     var rexpr = expr
     val dprec = 200 // precision *2
@@ -159,7 +158,7 @@ object Eval {
         println(l)
         return ;
       }
-      rexpr = refine(rexpr)(Context[VarDomain](new RoundingContext(0, dprec)))
+      rexpr = evaluator.refine(rexpr)(Context[VarDomain](new RoundingContext(0, dprec)))
 
     }
   }
@@ -183,7 +182,7 @@ object Eval {
         println(l)
         return ;
       }
-      rexpr = refine(rexpr)(Context[VarDomain](new RoundingContext(0, dprec)))
+      rexpr = evaluator.refine(rexpr)(Context[VarDomain](new RoundingContext(0, dprec)))
 
     }
   }
